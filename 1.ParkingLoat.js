55913531db515e31e8370bcbcb91979202e8fb9a
iamshivamishra <alwayshivamishra@gmail.com>
2026-06-01 17:56:38 +0530

    Added Parking Lot LLD implementation

---
+++ 1.ParkingLoat.js
@@ -0,0 +1,52 @@
+let ParkingLot = (()=> {
+  let instance
+
+  class ParkingLot {
+    constructor(totalSlots, gates){
+      this.totalSlots = totalSlots;
+      this.availableSlots = totalSlots;
+      this.gates = gates;
+      this.parkedVehicles = new Map();
+      this.nextSlot = 0;
+    }
+
+     parkVehicle(vehicleNumber) {
+       if(this.availableSlots <= 0){
+         return 'No available slots'
+       }
+       this.parkedVehicles.set(this.nextSlot, vehicleNumber);
+       this.availableSlots--;
+       return `Vehicle ${vehicleNumber} parked at slot ${this.nextSlot++}`
+     }
+     removeVehicle(vehicleNumber) {
+      for(let [slot, number] of this.parkedVehicles){
+        if(number === vehicleNumber){
+          this.parkedVehicles.delete(slot);
+          this.availableSlots++;
+          return `Vehicle ${vehicleNumber} removed from slot ${slot}`
+        }
+      }
+      return `Vehicle ${vehicleNumber} not found`
+     }
+     getAvailableSlots() {
+      return this.availableSlots;
+     }
+  }
+  return {
+    getInstance: (totalSlots, gates) => {
+      if(!instance){
+        instance = new ParkingLot(totalSlots, gates);
+      }
+      return instance;
+    }
+  }
+})();
+
+
+
+// Test Case
+
+let lot = ParkingLot.getInstance(100, [{entry: true, exit: true}]);
+console.log(lot.parkVehicle('KA-01-HH-1234')); // Vehicle KA-01-HH-1234 parked at slot 0
+console.log(lot.removeVehicle('KA-01-HH-1234')); // Vehicle KA-01-HH-1234 removed from slot 0
+console.log(lot.getAvailableSlots()); // 100  
